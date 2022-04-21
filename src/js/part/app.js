e5d89d1113576ca1f1381f1057f5ad8df65f0607
Onepo1ntFive <name.diesel@gmail.com>
2022-04-21 22:01:11 +0300

    Some changes

--- src/js/part/app.js
+++ src/js/part/app.js
@@ -1,3 +1,18 @@
-(function() {
-// ready
+(function () {
+    // footer menu
+    if (document.querySelector('.js-footer-menu-title')) {
+        let thisItem = null;
+        let thisContent = null;
+        let footerMenuTitles = document.querySelectorAll('.js-footer-menu-title');
+        Array.prototype.forEach.call(footerMenuTitles, (footerMenuTitle) => {
+            footerMenuTitle.addEventListener('click', (event) => { 
+                event.preventDefault();
+                thisItem = event.target;
+                thisContent = thisItem.nextElementSibling;
+
+                thisItem.classList.toggle('active');
+                slideToggle(thisContent);
+            })
+        })
+    }
 })();
