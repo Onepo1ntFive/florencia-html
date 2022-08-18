8673aff2539a51ee737050c563f4978d8c9ddf62
Onepo1ntFive <name.diesel@gmail.com>
2022-08-18 19:42:27 +0300

    Fixed block on order confirm

--- src/js/part/cart.js
+++ src/js/part/cart.js
@@ -8,4 +8,46 @@
         },
     });
 
+    let scrollPos = window.scrollY;
+
+    const stickyBlock = document.querySelector('.js-sticky-block');
+    const stickyContainer = document.querySelector('.js-sticky-container');
+    let stickyBlockTop = stickyBlock.getBoundingClientRect().top + scrollPos;
+    let stickyBlockBottom = stickyBlock.getBoundingClientRect().top + stickyBlock.offsetHeight + scrollPos;
+    let stickyBlockLeft = stickyContainer.getBoundingClientRect().left;
+    let stickyBlockWidth = stickyContainer.offsetWidth;
+
+    let stickyTopStop = stickyContainer.getBoundingClientRect().top + scrollPos;
+    let stickyBotStop = stickyContainer.getBoundingClientRect().top + stickyContainer.offsetHeight + scrollPos;
+
+    if (window.innerWidth >= 1240) {
+        window.onscroll = () => {
+            scrollPos = window.scrollY;
+            stickyBlockTop = stickyBlock.getBoundingClientRect().top + scrollPos;
+            stickyBlockBottom = stickyBlock.getBoundingClientRect().top + stickyBlock.offsetHeight + scrollPos;
+            stickyBlockLeft = stickyContainer.getBoundingClientRect().left;
+            stickyBlockWidth = stickyContainer.offsetWidth;
+
+            stickyTopStop = stickyContainer.getBoundingClientRect().top + scrollPos;
+            stickyBotStop = stickyContainer.getBoundingClientRect().top + stickyContainer.offsetHeight + scrollPos;
+
+            stickyBlock.style.width = stickyBlockWidth + 'px';
+
+            if (scrollPos >= stickyTopStop && scrollPos + stickyBlock.offsetHeight <= stickyBotStop) {
+                stickyBlock.classList.add('fixed');
+                stickyBlock.classList.remove('abs');
+            } else if (scrollPos >= stickyTopStop && scrollPos + stickyBlock.offsetHeight >= stickyBotStop) {
+                stickyBlock.classList.add('abs');
+                stickyBlock.classList.remove('fixed');
+            } else {
+                stickyBlock.classList.remove('abs');
+                stickyBlock.classList.remove('fixed');
+
+            }
+        } else {
+            stickyBlock.classList.remove('abs');
+            stickyBlock.classList.remove('fixed');
+        }
+    }
+
 })();
